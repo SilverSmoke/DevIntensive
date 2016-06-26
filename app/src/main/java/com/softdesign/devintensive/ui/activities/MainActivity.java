ce8ee99be3a9ff82d0387cf0b6785a4608044e43
Andrey Belikov <belycov@rambler.ru>
2016-06-27 00:59:32 +0600

    Add views and etc & repair

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.softdesign.devintensive.ui.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
